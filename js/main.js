c8d3f74dfcadc5bdce54a0ecbcdcbb33cba4c718
Tobiasz06 <TobiaszBremer01@gmail.com>
2025-04-08 01:30:11 +0200

    Buy now updated and login page start

--- js/main.js
+++ js/main.js
@@ -64,3 +64,23 @@ const faqItems = document.querySelectorAll('.faq-item');
     });
 });
 
+// Password hide toggle
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.toggle-password').forEach(icon => {
+      icon.addEventListener('click', () => {
+        const targetId = icon.getAttribute('data-target');
+        const input = document.getElementById(targetId);
+  
+        if (input.type === "password") {
+          input.type = "text";
+          icon.classList.remove("fa-eye");
+          icon.classList.add("fa-eye-slash");
+        } else {
+          input.type = "password";
+          icon.classList.remove("fa-eye-slash");
+          icon.classList.add("fa-eye");
+        }
+      });
+    });
+});
+
